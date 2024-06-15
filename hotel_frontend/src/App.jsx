63765c0f75cb7c8bca1212a1f6ecfdc49b005824
Viraj Surve <virajsurve710@gmail.com>
2024-06-15 17:29:27 +0530

    minor changes

--- hotel_frontend/src/App.jsx
+++ hotel_frontend/src/App.jsx
@@ -1,20 +1,25 @@
-import Header from "./components/Header.jsx";
+import Header from "./components/Navbar/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Nav from "./components/Nav.jsx";
 import Appp from "./components/Appp.jsx"
-
+import { Route,Routes } from "react-router-dom";
 
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Nav/>
-      <Appp/>
-
-      <Footer />
+      <Routes>
+        <Route index element={
+          <>
+            <Header />
+            <Nav/>
+            <Appp/>
+            <Footer />
+          </>
+        } />
+      </Routes>
     </>
   );
 }
+
 export default App;
