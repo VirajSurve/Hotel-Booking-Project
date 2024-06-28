c760a7b4dbc76915d660077cbebd9ea529cb143d
Viraj Surve <virajsurve710@gmail.com>
2024-06-28 18:55:30 +0530

    some fixes

--- hotel_frontend/src/components/Pages/PlacePage Components/images.jsx
+++ hotel_frontend/src/components/Pages/PlacePage Components/images.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
 
-function Images({ place }) {
+function Images({ place, showAllPhotos, setAllPhotos }) {
   return (
-    <div className="body">
-      <div className='Container'>
-        {place.photos.map((image, index) => {
-          let className = '';
-          switch (index) {
-            case 0:
-              className = 'house';
-              break;
-            case 1:
-              className = 'interior';
-              break;
-            case 2:
-              className = 'living-room';
-              break;
-            case 3:
-              className = 'kitchen';
-              break;
-            case 4:
-              className = 'bathroom';
-              break;
-            default:
-              className = '';
-          }
-          return (
-            <img 
-              key={index}
-              src={'http://localhost:4000/upload/' + image}
-              alt={className}
-              className={className}
-            />
-          );
-        })}
+    <div>
+      <div className="relative body overflow-hidden">
+        <div className='Container'>
+          {place.photos.map((image, index) => {
+            let className = '';
+            switch (index) {
+              case 0:
+                className = 'house aspect-square object-cover';
+                break;
+              case 1:
+                className = 'interior aspect-square object-cover';
+                break;
+              case 2:
+                className = 'living-room aspect-square object-cover';
+                break;
+              case 3:
+                className = 'kitchen aspect-square object-cover';
+                break;
+              case 4:
+                className = 'bathroom aspect-square object-cover';
+                break;
+              default:
+                className = 'hidden aspect-square object-cover';
+            }
+            return (
+              <img
+                key={index}
+                src={'http://localhost:4000/upload/' + image}
+                alt={className}
+                className={className}
+              />
+            );
+          })}
+        </div>
       </div>
+      <button onClick={() => {
+        console.log("Show more clicked");
+        setAllPhotos(true);
+      }} className='flex absolute bottom-4 right-40 bg-white px-3 py-2 rounded-2xl shadow shadow-md shadow-gray-500'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+        </svg>
+        Show more
+      </button>
     </div>
   );
 }
--- hotel_frontend/src/components/Pages/PlacePage.jsx
+++ hotel_frontend/src/components/Pages/PlacePage.jsx
@@ -1,34 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./PlacePage.css";
 import Title from "./PlacePage Components/Title";
 import Images from "./PlacePage Components/images";
 import Information from "./PlacePage Components/Information";
 import Reviews from "./PlacePage Components/Reviews";
 
-export default function PlacesPage(){
-    const {id}=useParams();
-    const [place,setPlace]=useState(null);
+export default function PlacesPage() {
+    const { id } = useParams();
+    const [place, setPlace] = useState(null);
+    const [showAllPhotos, setAllPhotos] = useState(false);
 
-    useEffect(()=>{
-        if(id){
-            axios.get(`http://localhost:4000/places/${id}`).then(response=>{
+    useEffect(() => {
+        if (id) {
+            axios.get(`http://localhost:4000/places/${id}`).then(response => {
                 setPlace(response.data);
             })
         }
-    },[id]);
+    }, [id]);
 
-    if(!place) return "";
+    if (!place) return "";
 
-    return(
+    console.log("showAllPhotos:", showAllPhotos);
+
+    // if (showAllPhotos) {
+    //     return (
+    //         <div className='fixed bg-black min-w-full min-h-screen flex items-center justify-center'>
+    //             Hi    
+    //         </div>
+    //     );
+    // }
+
+    return (
         <div className="mt-5">
             <div className='parent-container'>
-            <Title place={place} />
-            <Images place={place}/>
-            <Information place={place}/>
-            <Reviews place={place}/>
-            
+                <Title place={place} />
+                <Images place={place} showAllPhotos={showAllPhotos} setAllPhotos={setAllPhotos} />
+                <Information place={place} />
+                <Reviews place={place} />
             </div>
         </div>
     );
