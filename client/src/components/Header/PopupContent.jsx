c70b19e0c958ffa7ea4bfeca44e63bbe99ca2613
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2024-07-22 22:46:00 +0530

    mahit nahi

--- client/src/components/Header/PopupContent.jsx
+++ client/src/components/Header/PopupContent.jsx
@@ -1,73 +1,64 @@
 // PopupContent.jsx
-import React , {useState} from 'react';
-import { Link } from 'react-router-dom';
-import './PopupContent.css'; 
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./PopupContent.css";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../../UserContext";
 
 function PopupContent({ close }) {
+  const { ready, user, setUser } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
 
-const {ready,user,setUser}=useContext(UserContext);
-const [redirect,setRedirect]=useState(null);
-
-
-async function logout(){
+  async function logout() {
     axios.post("http://localhost:4000/logout");
     setUser(null);
     setRedirect("/");
-}
+  }
 
-if(!user && !redirect){
-  return <Navigate to={"/login"} />
-}
+  if (!user && !redirect) {
+    return <Navigate to={"/login"} />;
+  }
 
-if(redirect){
-  return <Navigate to={redirect} />
-}
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
 
-let {subpage}=useParams();
+  let { subpage } = useParams();
 
-if(subpage===undefined){
-  subpage="profile";
-}
-
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
 
   return (
     <div className="modal" onClick={() => close()}>
-    <div className="content">
-        <Link to ="/account">
-      <p className='font-medium'>My Profile</p>
-      </Link>
-      <Link to = "/account/bookings">
-      <p className='font-medium'>My Booking</p>
-      </Link>
-      <Link to = "/account/places">
-      <p className='font-medium'>My Accomodation</p>
-      </Link>
-      <Link to="/wishlist">
-  <p className='wishlist font-medium '>Wishlist</p>
-    </Link>
+      <div className="content">
+        <Link to="/account">
+          <p className="font-medium">My Profile</p>
+        </Link>
+        <Link to="/account/bookings">
+          <p className="font-medium">My Booking</p>
+        </Link>
+        <Link to="/account/places">
+          <p className="font-medium">My Accomodation</p>
+        </Link>
+        <Link to="/wishlist">
+          <p className="wishlist font-medium ">Wishlist</p>
+        </Link>
 
-<hr />
-<p>Help Center</p>
+        <hr />
+        <p>Help Center</p>
 
-{subpage==='profile' && (
-            <p>
-                <button onClick={logout}>
-                    Logout
-                </button>
-                </p>
+        {subpage === "profile" && (
+          <p>
+            <button onClick={logout}>Logout</button>
+          </p>
         )}
+      </div>
     </div>
-
-  </div>
-  )
+  );
 }
 
 export default PopupContent;
-
-
-
