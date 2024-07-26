a320b85556ac59897800f396fedcf03e4c04a3de
Viraj Surve <virajsurve710@gmail.com>
2024-07-26 16:27:07 +0530

    for dhana

--- client/src/components/Pages/PlacePage Components/BookingsPage.jsx
+++ client/src/components/Pages/PlacePage Components/BookingsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import AccountNav from "../../../AccountNav";
 import axios from "axios";
-
+// bc dhana
 
 export default function BookingsPage(){
     const [bookings,setBookings]=useState([]);
