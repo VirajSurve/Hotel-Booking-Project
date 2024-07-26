1e25e969b01ff6dcf6ebc74c16da8dccc180a277
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2024-07-27 00:22:01 +0530

    done bro

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <>
       <div className="relative p-4 min-h-screen">
-        {/* <Header /> */}
+        <Header />
         <Outlet />
         <MainPageFooter />
       </div>
--- client/src/components/Pages/PlacePage Components/Mapp.jsx
+++ client/src/components/Pages/PlacePage Components/Mapp.jsx
@@ -1,26 +1,30 @@
-// import React, { useEffect, useRef } from 'react';
-// import L from 'leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import pinIcon from './pin-location-4355.svg'
-// const Mapp = ({place}) => {
+// import React, { useEffect, useRef } from "react";
+// import L from "leaflet";
+// import "leaflet/dist/leaflet.css";
+// import pinIcon from "./pin-location-4355.svg";
+// const Mapp = ({ place }) => {
 //   const mapContainerRef = useRef(null);
 //   const mapRef = useRef(null);
 
 //   useEffect(() => {
 //     if (mapContainerRef.current && !mapRef.current) {
-//       mapRef.current = L.map(mapContainerRef.current).setView([place.X, place.Y], 20);
-//       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       mapRef.current = L.map(mapContainerRef.current).setView(
+//         [place.X, place.Y],
+//         20
+//       );
+//       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 //       }).addTo(mapRef.current);
 
 //       const data = {
 //         pict: {
-//           coords: [place.X,place.Y],
+//           coords: [place.X, place.Y],
 //           title: "Pict pune",
 //           address: `<b>Pict pune</b><br>123<br>PUNE<br>`,
-//           website: 'https://google.com',
+//           website: "https://google.com",
 //           phone: 123,
-//         }
+//         },
 //       };
 //       const customIcon = L.icon({
 //         iconUrl: pinIcon,
@@ -55,22 +59,27 @@
 //   }, []);
 //   // Inline styles for centering the map container
 //   const outerContainerStyle = {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     height: '100vh',
-//     backgroundColor: 'white',
-//     marginTop:'170px',
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     height: "100vh",
+//     backgroundColor: "white",
+//     marginTop: "170px",
 //     // Optional background color
 //   };
 //   const mapStyle = {
-//     width: '1000px',
-//     height: '500px',
+//     width: "1000px",
+//     height: "500px",
 //     // margin:'10px',
 //   };
 //   return (
-//     <div style={outerContainerStyle} >
-//       <div ref={mapContainerRef} style={mapStyle} id="map" className='rounded-xl'/>
+//     <div style={outerContainerStyle}>
+//       <div
+//         ref={mapContainerRef}
+//         style={mapStyle}
+//         id="map"
+//         className="rounded-xl"
+//       />
 //     </div>
 //   );
 // };
